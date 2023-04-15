4b4695926a6405ce454d792089eecc0f203dc7f8
James Crutchley <jamescrutchley98@gmail.com>
2023-04-15 20:45:30 +1000

    setup express

--- server.js
+++ server.js
@@ -2,3 +2,8 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('_site'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
